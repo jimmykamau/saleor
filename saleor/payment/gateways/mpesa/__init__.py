670ee387edcd6ac8dcb943cec1c10b9c7fccca9f
Jimmy Kamau <jimmykambiz@gmail.com>
2020-04-10 22:49:43 +0300

    Confirm Mpesa payment before accepting transaction

--- saleor/payment/gateways/mpesa/__init__.py
+++ saleor/payment/gateways/mpesa/__init__.py
@@ -1,11 +1,13 @@
-import uuid
 import json
 import logging
+import time
+import uuid
+
 import requests
 from django.core.cache import cache
 from django.utils import timezone
 
-from ... import ChargeStatus, TransactionKind
+from ... import ChargeStatus, TransactionError, TransactionKind
 from ...interface import GatewayConfig, GatewayResponse, PaymentData
 from .utils import generate_lipa_password, get_access_token
 
@@ -30,20 +32,21 @@ def get_billing_data(payment_information: PaymentData, config: GatewayConfig):
     timestamp = timezone.now().strftime("%Y%m%d%H%M%S")
     password = generate_lipa_password(timestamp, config)
     callback_url = config.connection_params['callback_url']
-    reference = get_client_token()
+    phone_number = payment_information.billing.phone[1:]
+    token = payment_information.order_id if payment_information.order_id else payment_information.token
 
     return dict(
         BusinessShortCode=shortcode,
-        Password=password,
+        Password=password.decode('utf-8'),
         Timestamp=timestamp,
         TransactionType="CustomerPayBillOnline",
-        Amount=payment_information.amount,
-        PartyA=payment_information.billing.phone,
+        Amount=int(payment_information.amount),
+        PartyA=phone_number,
         PartyB=shortcode,
-        PhoneNumber=payment_information.billing.phone,
+        PhoneNumber=phone_number,
         CallBackURL=callback_url,
-        AccountReference=reference,
-        TransactionDesc="Mpesa payment"
+        AccountReference=token,
+        TransactionDesc="M-PESA payment"
     )
 
 
@@ -68,25 +71,35 @@ def capture(payment_information: PaymentData, config: GatewayConfig) -> GatewayR
     error = None
     success = False
     action_required = False
+    response_data = None
+    billing_data = get_billing_data(payment_information, config)
+    access_token = _access_token(config)
     try:
         response = requests.post(
-            f"config.connection_params['base_url']mpesa/stkpush/v1/processrequest",
+            f"{config.connection_params['base_url']}mpesa/stkpush/v1/processrequest",
             headers={
-                "Authorization": f"Bearer {auth_key}",
+                "Authorization": f"Bearer {access_token}",
                 "Content-Type": "application/json"
             },
-            data=json.dumps(data)
+            data=json.dumps(billing_data)
         )
         response_data = response.json()
         response.raise_for_status()
     except Exception:
-        if response_data['errorMessage'] == "Invalid Access Token":
-            get_access_token(config)
-            capture(payment_information, config)
-        logger.warning(f"Error initiating Mpesa payment: {response_data}", exc_info=True)
-        error = response_data['errorMessage']
-        action_required = True
+        if response_data:
+            if response_data.get('errorMessage') == "Invalid Access Token":
+                get_access_token(config)
+                capture(payment_information, config)
+            else:
+                error = TransactionError.PROCESSING_ERROR
+                logger.warning(f"Error initiating Mpesa payment: {response_data}", exc_info=True)
+                action_required = True
+        else:
+            logger.warning(f"Error initiating MPESA payment", exc_info=True)
+            error = TransactionError.PROCESSING_ERROR
+            action_required = True
     else:
+        response_data['Timestamp'] = billing_data['Timestamp']
         success = True
 
     return GatewayResponse(
@@ -96,25 +109,67 @@ def capture(payment_information: PaymentData, config: GatewayConfig) -> GatewayR
         amount=payment_information.amount,
         currency=payment_information.currency,
         transaction_id=response_data.get('CheckoutRequestID', payment_information.token),
-        error=error
+        error=error,
+        raw_response=response_data
     )
 
 
-def confirm(payment_information: PaymentData, config: GatewayConfig) -> GatewayResponse:
+def confirm(payment_information: PaymentData, config: GatewayConfig, capture_response: GatewayResponse) -> GatewayResponse:
     """Perform confirm transaction."""
     error = None
-    success = dummy_success()
-    if not success:
-        error = "Unable to process capture"
+    response_data = None
+    action_required = False
+    success = False
+    access_token = _access_token(config)
+    timestamp = capture_response.raw_response['Timestamp']
+    password = generate_lipa_password(timestamp, config)
+    transaction_data = dict(
+        BusinessShortCode=config.connection_params['shortcode'],
+        Password=password.decode('utf-8'),
+        Timestamp=timestamp,
+        CheckoutRequestID=capture_response.raw_response['CheckoutRequestID']
+    )
+
+    try:
+        response = requests.post(
+            f"{config.connection_params['base_url']}mpesa/stkpushquery/v1/query",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Content-Type": "application/json"
+            },
+            data=json.dumps(transaction_data)
+        )
+        response_data = response.json()
+        response.raise_for_status()
+    except Exception:
+        if response_data:
+            if response_data.get('errorMessage') == "The transaction is being processed":
+                time.sleep(3)
+                confirm(payment_information, config, capture_response)
+            else:
+                error = TransactionError.PROCESSING_ERROR
+                logger.warning(f"Error confirming MPESA payment: {response_data}", exc_info=True)
+                action_required = True
+        else:
+            logger.warning(f"Error confirming MPESA payment", exc_info=True)
+            error = TransactionError.PROCESSING_ERROR
+            action_required = True
+    else:
+        if response_data['ResultDesc'] == "The service request is processed successfully.":
+            success = True
+        else:
+            error = TransactionError.DECLINED
+            action_required = True
 
     return GatewayResponse(
         is_success=success,
-        action_required=False,
-        kind=TransactionKind.CAPTURE,
+        action_required=action_required,
+        kind=TransactionKind.CONFIRM,
         amount=payment_information.amount,
         currency=payment_information.currency,
-        transaction_id=payment_information.token,
-        error=error
+        transaction_id=response_data.get('CheckoutRequestID', payment_information.token),
+        error=error,
+        raw_response=response_data
     )
 
 
@@ -138,4 +193,9 @@ def process_payment(
     payment_information: PaymentData, config: GatewayConfig
 ) -> GatewayResponse:
     """Process the payment."""
-    return capture(payment_information=payment_information, config=config)
+    capture_request = capture(payment_information=payment_information, config=config)
+    if capture_request.error:
+        return capture_request
+    time.sleep(10)
+    confirmation = confirm(payment_information, config, capture_request)
+    return confirmation
